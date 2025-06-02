e35eefde30a3d46864ef0e975c1c47edb734ca94
Sumit <raj.sumit9835@gmail.com>
2025-06-02 12:17:00 +0530

    back to successful transactions.

--- fed-learn-block/deploy.js
+++
@@ -1,5 +0,0 @@
-var abi = JSON.parse("'"${ABI}"'");
-var bin = "0x'"${BIN}"'";
-var Simple = eth.contract(abi);
-var tx = Simple.new({from: eth.accounts[0], data: bin, gas: 3000000});
-console.log("TxHash:", tx.transactionHash);
